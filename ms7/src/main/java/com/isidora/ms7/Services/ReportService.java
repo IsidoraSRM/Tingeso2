ac717acebd8820cb5dac00e2f26e9b6e35f30718
Isidora Reveco <164648232+IsidoraSRM@users.noreply.github.com>
2025-05-23 20:30:04 -0400

    ms7-problems

--- ms7/src/main/java/com/isidora/ms7/Services/ReportService.java
+++ ms7/src/main/java/com/isidora/ms7/Services/ReportService.java
@@ -2,9 +2,15 @@ package com.isidora.ms7.Services;
 
 
 import com.isidora.ms7.Entities.ReportEntity;
+import com.isidora.ms7.Models.ReservationEntity;
 import com.isidora.ms7.Repositories.ReportRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,10 +20,33 @@ public class ReportService {
 
     @Autowired
     ReportRepository reportRepository;
+    @Autowired
+    RestTemplate restTemplate;
+
+    // URL Reservations
+    public List<ReservationEntity> getAllReservations(){
+        ResponseEntity<List<ReservationEntity>> response = restTemplate.exchange(
+                "http://ms5/api/reservations/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ReservationEntity>>() {}
+        );
+        return response.getBody();
+    }
 
     public List<ReportEntity> getAllReports(){
-        return reportRepository.findAll();
+        List<ReportEntity> reports = reportRepository.findAll();
+        if(reports.isEmpty()) {
+            throw new EntityNotFoundException("No se encontraron reportes");
+            // O alternativamente: return Collections.emptyList();
+        }
+        return reports;
+
     }
+
+
+
+
 }
 
 
